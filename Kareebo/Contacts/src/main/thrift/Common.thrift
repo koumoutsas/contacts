8c84dd7b879ed75d295c646596c15c63dea3c103
Yannis Ioannidis <jfioannidis@gmail.com>
2015-05-31 23:41:25 +0200

    Thrift files and gradle plugin

---
+++ Kareebo/Contacts/src/main/thrift/Common.thrift
@@ -0,0 +1,52 @@
+namespace java org.kareebo.contacts.common
+
+typedef i64 Id;
+typedef binary ByteArray;
+
+struct UserAgent
+{
+    1: string platform,
+    2: string version,
+}
+
+struct Client
+{
+    1: UserAgent userAgent,
+    2: Id id,
+}
+
+enum Algorithm
+{
+    SHA256 = 1,
+    ECprime239v1 = 2,
+}
+
+
+struct CryptoBuffer
+{
+    1: ByteArray buffer,
+    2: Algorithm algorithm,
+}
+
+struct PublicKeys
+{
+    1: CryptoBuffer encryption,
+    2: CryptoBuffer verification,
+}
+
+enum HandleType
+{
+    EmailAddress = 1,
+}
+
+struct Handle
+{
+    1: string contents,
+    2: HandleType type,
+}
+
+struct HashedContact
+{
+    1: Id id,
+    2: CryptoBuffer hash,
+}
---
+++ Kareebo/Contacts/src/main/thrift/Services.thrift
@@ -0,0 +1,119 @@
+include "Client.thrift"
+include "Common.thrift"
+
+namespace java org.kareebo.contacts.thrift
+
+typedef string RegistrationCode
+
+struct IdPair
+{
+    1: Common.Id userId,
+    2: Common.Id clientId,
+}
+
+struct Signature
+{
+    1: Common.ByteArray signature,
+    2: IdPair ids,
+}
+
+typedef Common.ByteArray EncryptedData
+
+exception InvalidArgument
+{
+    1: string what,
+}
+
+service UserRegistration
+{
+    IdPair userRegistration1(1: Common.Handle handle, 2: Common.UserAgent userAgent, 3: Common.PublicKeys publicKeys) throws (1: InvalidArgument invalidArgument),
+    void userRegistration2(1: RegistrationCode code, 2: Signature signature) throws (1: InvalidArgument invalidArgument),
+}
+
+service DeviceRegistration
+{
+    IdPair deviceRegistration1(1: Common.Handle handle,2: Common.UserAgent userAgent, 3: Common.PublicKeys publicKeys) throws (1: InvalidArgument invalidArgument),
+    void deviceRegistration2(1: RegistrationCode code, 2: Signature signature) throws (1: InvalidArgument invalidArgument),
+}
+
+service LogInClient
+{
+    void logInClient1(1: IdPair ids) throws (1: InvalidArgument invalidArgument),
+    void logInClient2(1: RegistrationCode code, 2: Signature signature) throws (1: InvalidArgument invalidArgument),
+}
+
+service ModifyKeys
+{
+    void modifyKeys1(1: Common.PublicKeys newPublicKeys, 2: Signature signature) throws (1: InvalidArgument invalidArgument),
+}
+
+service ModifyUserAgent
+{
+    void modifyUserAgent1(1: Common.UserAgent userAgent, 2: Signature signature) throws (1: InvalidArgument invalidArgument),
+}
+
+service ModifyHandle
+{
+    void modifyHandle1(1: Common.Handle newHandle, 2: Signature signature) throws (1: InvalidArgument invalidArgument),
+    void modifyHandle2(1: RegistrationCode code, 2: Signature signature) throws (1: InvalidArgument invalidArgument),
+}
+
+service TestKeys
+{
+    EncryptedData testKeys1(1: string fixed, 2: IdPair ids) throws (1: InvalidArgument invalidArgument),
+    bool testKeys2(1: string fixed, 2: Signature signature) throws (1: InvalidArgument invalidArgument),
+}
+
+enum ContactOperationType
+{
+    Add = 1,
+    Delete = 2,
+}
+
+struct ContactOperation
+{
+    1: Common.HashedContact contact,
+    2: ContactOperationType type,
+}
+
+typedef set<ContactOperation> ContactOperationSet
+
+service UpdateServerContactBook
+{
+    void updateServerContactBook1(1: ContactOperationSet contactOperationSet, 2: Signature signature) throws (1: InvalidArgument invalidArgument),
+}
+
+struct EncryptedContact
+{
+    1: Common.HashedContact hashedContact,
+    2: Common.CryptoBuffer encrypted,
+}
+
+typedef set<EncryptedContact> EncryptedContactSet
+
+service UpdateLocalContactBook
+{
+    EncryptedContactSet updateLocalContactBook1(1: string fixed, 2: Signature signature) throws (1: InvalidArgument invalidArgument),
+}
+
+struct EncryptionKey
+{
+    1: Common.Id clientId,
+    2: Common.CryptoBuffer encryption,
+}
+
+typedef set<EncryptionKey> EncryptionKeySet
+
+struct UploadedContact
+{
+    1: IdPair ids,
+    2: Common.CryptoBuffer encrypted,
+}
+
+typedef set<UploadedContact> UploadedContactSet
+
+service UploadEncryptedContactCard
+{
+    EncryptionKeySet uploadEncryptedContactCard1(1: Common.Id userId, 2: Signature signature) throws (1: InvalidArgument invalidArgument),
+    void uploadEncryptedContactCard2(1: UploadedContactSet uploadedContactSet, 2: Signature signature) throws (1: InvalidArgument invalidArgument),
+}

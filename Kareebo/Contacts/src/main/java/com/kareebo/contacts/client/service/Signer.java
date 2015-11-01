6c1713cb3ad178c05b19420e2d3468e3a3293a72
Yannis Ioannidis <jfioannidis@gmail.com>
2015-11-01 23:31:51 +0100

    Signer client class

---
+++ Kareebo/Contacts/src/main/java/com/kareebo/contacts/client/service/Signer.java
@@ -0,0 +1,38 @@
+package com.kareebo.contacts.client.service;
+
+import com.kareebo.contacts.client.SigningKey;
+import com.kareebo.contacts.crypto.Utils;
+import com.kareebo.contacts.thrift.ClientId;
+import com.kareebo.contacts.thrift.SignatureBuffer;
+import org.apache.thrift.TBase;
+import org.apache.thrift.TException;
+import org.apache.thrift.TSerializer;
+
+import java.security.*;
+
+/**
+ * Base class for signing Thrift objects
+ */
+class Signer
+{
+	private final SigningKey signingKey;
+	private final ClientId clientId;
+
+	Signer(final SigningKey signingKey,final ClientId clientId)
+	{
+		this.signingKey=signingKey;
+		this.clientId=clientId;
+	}
+
+	SignatureBuffer sign(final TBase object) throws TException, NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, SignatureException
+	{
+		final Signature ecdsaSign=Signature.getInstance(Utils.getSignatureAlgorithm(signingKey.algorithm),Utils.getProvider());
+		ecdsaSign.initSign(signingKey.key);
+		ecdsaSign.update(new TSerializer().serialize(object));
+		final SignatureBuffer signatureBuffer=new SignatureBuffer();
+		signatureBuffer.setBuffer(ecdsaSign.sign());
+		signatureBuffer.setAlgorithm(signingKey.algorithm);
+		signatureBuffer.setClient(clientId);
+		return signatureBuffer;
+	}
+}
---
+++ Kareebo/Contacts/src/test/java/com/kareebo/contacts/client/service/KeyPair.java
@@ -0,0 +1,35 @@
+package com.kareebo.contacts.client.service;
+
+import com.kareebo.contacts.crypto.Utils;
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.spec.ECParameterSpec;
+
+import java.security.*;
+
+/**
+ * Utility class for generating a key pair
+ */
+class KeyPair
+{
+	private final java.security.KeyPair keyPair;
+
+	KeyPair() throws InvalidAlgorithmParameterException, NoSuchProviderException, NoSuchAlgorithmException
+	{
+		Security.addProvider(new BouncyCastleProvider());
+		final ECParameterSpec ecSpec=ECNamedCurveTable.getParameterSpec("prime192v1");
+		final KeyPairGenerator g=KeyPairGenerator.getInstance("ECDSA",Utils.getProvider());
+		g.initialize(ecSpec,new SecureRandom());
+		keyPair=g.generateKeyPair();
+	}
+
+	PrivateKey getPrivate()
+	{
+		return keyPair.getPrivate();
+	}
+
+	PublicKey getPublic()
+	{
+		return keyPair.getPublic();
+	}
+}
